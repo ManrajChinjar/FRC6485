4966c6176cffdfaa6bbdd10f41dda6667c3c1106
macedir <thoughtblocked@gmail.com>
2017-02-20 23:16:33 -0800

    Fix Bridge lowering PWM rate
    Should be negative.

--- Deployer/src/org/usfirst/frc/team6485/robot/RobotMap.java
+++ Deployer/src/org/usfirst/frc/team6485/robot/RobotMap.java
@@ -38,7 +38,7 @@ public class RobotMap {
 
   public static final double BRIDGE_RAISEPWM = 0.22;
 
-  public static final double BRIDGE_LOWERPWM = 0.16;
+  public static final double BRIDGE_LOWERPWM = -0.16;
 
   public static final double BRIDGE_MAXSAFEPWM = 0.21;
 
