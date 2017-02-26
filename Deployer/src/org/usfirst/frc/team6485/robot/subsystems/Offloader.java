8f5843341b905446c5bcc87665041b73d2a673bf
macedir <thoughtblocked@gmail.com>
2017-02-25 21:39:04 -0800

    Increase offloader motor stall magnitude
    Lifting a full container of balls may raise the current enough to trip a
    1.5 amp check. Change the magnitude check as needed so that it only
    trips when the actual vinyl is taut, and not when its trying to lift
    balls up.

--- Deployer/src/org/usfirst/frc/team6485/robot/subsystems/Offloader.java
+++ Deployer/src/org/usfirst/frc/team6485/robot/subsystems/Offloader.java
@@ -42,7 +42,7 @@ public class Offloader extends Subsystem {
   }
 
   public void updateState() {
-    if (Math.abs(Math.abs(getCurrent()) - Math.abs(RobotMap.OFFLOADER_MAXWORKINGCURRENT)) > 1.5
+    if (Math.abs(Math.abs(getCurrent()) - Math.abs(RobotMap.OFFLOADER_MAXWORKINGCURRENT)) > 5.0
         && getSpeed() < 0.0) {
       mState = OFFLOADER_STATE.TAUT;
     } else if (getSpeed() > 0.0) {
